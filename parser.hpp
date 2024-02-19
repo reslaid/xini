19846f8c8c85c8cced1250f1318a5aa09d599607
Hayes <154455366+reslaid@users.noreply.github.com>
2024-02-19 15:04:02 +0200

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -1,3 +1,4 @@
+
 #ifndef					__IniParser_Required_
 # define                    __IniParser_Required_
 
@@ -24,40 +25,40 @@
  */
 # define IniParser          IniParserW
 
- /**
-  * @brief Macro that defines the string type used by IniParser based on whether the _UNICODE macro is defined.
-  *
-  * If _UNICODE is defined, IniString is defined as std::wstring, which is the wide-character string type.
-  * Otherwise, IniString is defined as std::string, which is the ASCII-character string type.
-  */
+/**
+ * @brief Macro that defines the string type used by IniParser based on whether the _UNICODE macro is defined.
+ *
+ * If _UNICODE is defined, IniString is defined as std::wstring, which is the wide-character string type.
+ * Otherwise, IniString is defined as std::string, which is the ASCII-character string type.
+ */
 # define IniString          std::wstring
 
-  /**
-   * @brief Macro that defines the character type used by IniParser based on whether the _UNICODE macro is defined.
-   *
-   * If _UNICODE is defined, IniChar is defined as wchar_t, which is the wide-character type.
-   * Otherwise, IniChar is defined as char, which is the ASCII-character type.
-   */
+/**
+ * @brief Macro that defines the character type used by IniParser based on whether the _UNICODE macro is defined.
+ *
+ * If _UNICODE is defined, IniChar is defined as wchar_t, which is the wide-character type.
+ * Otherwise, IniChar is defined as char, which is the ASCII-character type.
+ */
 # define IniChar            wchar_t
 
-   /**
-	* @brief Macro that converts the input string literal to a wide-character string literal if _UNICODE is defined.
-	*
-	* If _UNICODE is defined, the input string literal is prefixed with 'L', indicating it's a wide-character string literal.
-	* Otherwise, the input string literal remains unchanged.
-	*
-	* @param x The input string literal.
-	*/
+/**
+* @brief Macro that converts the input string literal to a wide-character string literal if _UNICODE is defined.
+*
+* If _UNICODE is defined, the input string literal is prefixed with 'L', indicating it's a wide-character string literal.
+* Otherwise, the input string literal remains unchanged.
+*
+* @param x The input string literal.
+*/
 # define IniT(x)            L##x
 
-	/**
-	 * @brief Macro that converts the input value to a string literal if _UNICODE is defined.
-	 *
-	 * If _UNICODE is defined, the input value is converted to a string literal using the '#' preprocessor operator.
-	 * Otherwise, the input value remains unchanged.
-	 *
-	 * @param x The input value.
-	 */
+/**
+ * @brief Macro that converts the input value to a string literal if _UNICODE is defined.
+ *
+ * If _UNICODE is defined, the input value is converted to a string literal using the '#' preprocessor operator.
+ * Otherwise, the input value remains unchanged.
+ *
+ * @param x The input value.
+ */
 # define IniStringify(x)    L#x
 
 #else
@@ -70,58 +71,58 @@
  */
 # define IniParser          IniParserA
 
- /**
-  * @brief Macro that defines the string type used by IniParser based on whether the _UNICODE macro is defined.
-  *
-  * If _UNICODE is defined, IniString is defined as std::wstring, which is the wide-character string type.
-  * Otherwise, IniString is defined as std::string, which is the ASCII-character string type.
-  */
+/**
+ * @brief Macro that defines the string type used by IniParser based on whether the _UNICODE macro is defined.
+ *
+ * If _UNICODE is defined, IniString is defined as std::wstring, which is the wide-character string type.
+ * Otherwise, IniString is defined as std::string, which is the ASCII-character string type.
+ */
 # define IniString          std::string
 
-  /**
-   * @brief Macro that defines the character type used by IniParser based on whether the _UNICODE macro is defined.
-   *
-   * If _UNICODE is defined, IniChar is defined as wchar_t, which is the wide-character type.
-   * Otherwise, IniChar is defined as char, which is the ASCII-character type.
-   */
+/**
+ * @brief Macro that defines the character type used by IniParser based on whether the _UNICODE macro is defined.
+ *
+ * If _UNICODE is defined, IniChar is defined as wchar_t, which is the wide-character type.
+ * Otherwise, IniChar is defined as char, which is the ASCII-character type.
+ */
 # define IniChar            char
 
-   /**
-	* @brief Macro that converts the input string literal to a wide-character string literal if _UNICODE is defined.
-	*
-	* If _UNICODE is defined, the input string literal is prefixed with 'L', indicating it's a wide-character string literal.
-	* Otherwise, the input string literal remains unchanged.
-	*
-	* @param x The input string literal.
-	*/
+/**
+* @brief Macro that converts the input string literal to a wide-character string literal if _UNICODE is defined.
+*
+* If _UNICODE is defined, the input string literal is prefixed with 'L', indicating it's a wide-character string literal.
+* Otherwise, the input string literal remains unchanged.
+*
+* @param x The input string literal.
+*/
 # define IniT(x)            x
 
-	/**
-	 * @brief Macro that converts the input value to a string literal if _UNICODE is defined.
-	 *
-	 * If _UNICODE is defined, the input value is converted to a string literal using the '#' preprocessor operator.
-	 * Otherwise, the input value remains unchanged.
-	 *
-	 * @param x The input value.
-	 */
+/**
+ * @brief Macro that converts the input value to a string literal if _UNICODE is defined.
+ *
+ * If _UNICODE is defined, the input value is converted to a string literal using the '#' preprocessor operator.
+ * Otherwise, the input value remains unchanged.
+ *
+ * @param x The input value.
+ */
 # define IniStringify(x)    #x
 
 #endif
 
-	 /**
-	  * @brief A macro that defines the type for INI file data.
-	  *
-	  * This macro defines the type for storing INI file data. It is a map of maps, where the outer map
-	  * represents the sections of the INI file, and the inner maps represent the key-value pairs within
-	  * each section. The type of the keys and values in the maps is determined by the IniString type.
-	  */
+/**
+ * @brief A macro that defines the type for INI file data.
+ *
+ * This macro defines the type for storing INI file data. It is a map of maps, where the outer map
+ * represents the sections of the INI file, and the inner maps represent the key-value pairs within
+ * each section. The type of the keys and values in the maps is determined by the IniString type.
+ */
 # define IniMap             std::map<IniString, std::map<IniString, IniString>>
 
 #pragma endregion
 
-	  /**
-	   * @brief A simple INI file parser class. <ASCII>
-	   */
+/**
+ * @brief A simple INI file parser class. <ASCII>
+ */
 class IniParserA {
 public:
 	/**
